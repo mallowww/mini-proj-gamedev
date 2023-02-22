dc3157575c8d2be59c9c3cfead05c80b22916f49
mell <chanon4817@gmail.com>
2023-02-22 20:47:16 +0700

    :tada: Init

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "What should I name this game btw")
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 1920, 1080
+}
+
+func main() {
+	ebiten.SetWindowSize(1920, 1080)
+	ebiten.SetWindowTitle("This game is just like a whiteboard")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
+}
