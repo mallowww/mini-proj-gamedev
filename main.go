5d176a52826c2dc1876360af29a1374391b40d60
mell <chanon4817@gmail.com>
2023-02-22 21:19:33 +0700

    feat: refac global const, update event state in title

--- main.go
+++ main.go
@@ -1,51 +1,83 @@
 package main
 
 import (
-	"image"
-	"log"
-	"os"
+    "image"
+    "log"
+    "os"
 
-	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-	"golang.org/x/image/draw"
+    "github.com/hajimehoshi/ebiten/v2"
+    "github.com/hajimehoshi/ebiten/v2/ebitenutil"
+    "golang.org/x/image/draw"
 )
 
-type Game struct{}
+const (
+    screenWidth  = 800
+    screenHeight = 600
+)
+
+type GameState int
+
+const (
+    MenuState GameState = iota
+    PlayState
+)
+
+type Game struct {
+    state GameState
+}
 
 func (g *Game) Update() error {
-	return nil
+    // update the game state based on user input or other events
+    switch g.state {
+    case MenuState:
+        // update the menu state
+    case PlayState:
+        // update the play state
+    }
+
+    return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "What should I name this game btw")
+    switch g.state {
+    case MenuState:
+        ebitenutil.DebugPrint(screen, "Menu")
+    case PlayState:
+        ebitenutil.DebugPrint(screen, "Play")
+    }
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+    return screenWidth, screenHeight
 }
 
 func main() {
-	iconFile, err := os.Open("assets/img/title-icon.png")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer iconFile.Close()
+    iconFile, err := os.Open("assets/img/title-icon.png")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer iconFile.Close()
 
-	iconImg, _, err := image.Decode(iconFile)
-	if err != nil {
-		log.Fatal(err)
-	}
+    iconImg, _, err := image.Decode(iconFile)
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	// resize title icon config
-	const iconSize = 128
-	resizedImg := image.NewRGBA(image.Rect(0, 0, iconSize, iconSize))
-	draw.NearestNeighbor.Scale(resizedImg, resizedImg.Bounds(), iconImg, iconImg.Bounds(), draw.Over, nil)
+    // resize title icon config
+    const iconSize = 128
+    resizedImg := image.NewRGBA(image.Rect(0, 0, iconSize, iconSize))
+    draw.NearestNeighbor.Scale(resizedImg, resizedImg.Bounds(), iconImg, iconImg.Bounds(), draw.Over, nil)
 
-	ebiten.SetWindowSize(800, 600)
-	ebiten.SetWindowTitle("This game is just like a whiteboard")
-	ebiten.SetWindowIcon([]image.Image{resizedImg}) // Set the icon image
+    ebiten.SetWindowSize(screenWidth, screenHeight)
+    ebiten.SetWindowTitle("Game Title")
+    ebiten.SetWindowIcon([]image.Image{resizedImg}) // Set the icon image
 
-	if err := ebiten.RunGame(&Game{}); err != nil {
-		log.Fatal(err)
-	}
+    // initialize the game state
+    game := &Game{
+        state: MenuState,
+    }
+
+    if err := ebiten.RunGame(game); err != nil {
+        log.Fatal(err)
+    }
 }
